f19872c57cc050136e9c462e462aae920415b040
kerta1n <36344851+kerta1n@users.noreply.github.com>
2023-09-09 17:37:03 -0400

    test flutter

--- cshp-fltr/lib/main.dart
+++ cshp-fltr/lib/main.dart
@@ -57,11 +57,15 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          '\$0', // Default number display
-          style: TextStyle(fontSize: 36.0), // Adjust the font size as needed
-        ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Placeholder(), // Replace with your first page content
+          Placeholder(), // Replace with your second page content
+          ThirdPage(), // Third page content with expanded numpad, amount counter, and currency switcher
+          Placeholder(), // Replace with your fourth page content
+          Placeholder(), // Replace with your fifth page content
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
@@ -91,120 +95,153 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedItemColor: Colors.white,
         backgroundColor: Colors.grey[900], // Dark background for the navbar
       ),
-      bottomSheet: Container(
-        color: Colors.grey[900], // Dark background
-        padding: EdgeInsets.all(16.0), // Add padding
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end, // Align to the bottom of the screen
+      children: <Widget>[
+        SizedBox(height: MediaQuery.of(context).size.height * 0.20), // Move amount counter up by 20% of screen height
+        Center(
+          child: Text(
+            '\$0', // Default number display
+            style: TextStyle(fontSize: 36.0), // Adjust the font size as needed
+          ),
+        ),
+        SizedBox(height: 16.0), // Add spacing
+        Container(
+          padding: EdgeInsets.all(16.0), // Add padding around the currency switcher
+          decoration: BoxDecoration(
+            color: Colors.grey[900], // Dark background
+            borderRadius: BorderRadius.circular(8.0), // Rounded dropdown
+          ),
+          child: DropdownButton<String>(
+            value: 'USD', // Default currency
+            onChanged: (newValue) {
+              // Handle currency change
+            },
+            items: <String>['USD', 'EUR', 'GBP', 'JPY', 'CAD']
+                .map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+        SizedBox(height: 8.0), // Add spacing
+        Expanded(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 2.0, // Adjust the aspect ratio for horizontal expansion
+            ),
+            shrinkWrap: true,
+            itemCount: 12, // Added buttons 0, 9, and '<'
+            itemBuilder: (context, index) {
+              if (index == 9) {
+                return ElevatedButton(
+                  onPressed: () {
+                    // Handle the "0" button tap
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0.0), // No rounded corners
+                      ),
+                    ),
+                  ),
+                  child: Text('0'),
+                );
+              } else if (index == 10) {
+                return ElevatedButton(
+                  onPressed: () {
+                    // Handle the "." button tap
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0.0), // No rounded corners
+                      ),
+                    ),
+                  ),
+                  child: Text('.'),
+                );
+              } else if (index == 11) {
+                return ElevatedButton(
+                  onPressed: () {
+                    // Handle the backspace button tap
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0.0), // No rounded corners
+                      ),
+                    ),
+                  ),
+                  child: Text('<'),
+                );
+              } else {
+                return ElevatedButton(
+                  onPressed: () {
+                    // Handle number button tap
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0.0), // No rounded corners
+                      ),
+                    ),
+                  ),
+                  child: Text('${index + 1}'), // Adjusted index
+                );
+              }
+            },
+          ),
+        ),
+        SizedBox(height: 16.0), // Add spacing
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(16.0), // Add padding around the currency switcher
-              decoration: BoxDecoration(
-                color: Colors.grey[900], // Dark background
-                borderRadius: BorderRadius.circular(8.0), // Rounded dropdown
-              ),
-              child: DropdownButton<String>(
-                value: 'USD', // Default currency
-                onChanged: (newValue) {
-                  // Handle currency change
-                },
-                items: <String>['USD', 'EUR', 'GBP', 'JPY', 'CAD']
-                    .map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ),
-            GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 1.0, // Square buttons
-              ),
-              shrinkWrap: true,
-              itemCount: 12, // Added buttons 0, 9, and '<'
-              itemBuilder: (context, index) {
-                if (index == 9) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      // Handle the "0" button tap
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
-                    ),
-                    child: Text('0'),
-                  );
-                } else if (index == 10) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      // Handle the "." button tap
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
-                    ),
-                    child: Text('.'),
-                  );
-                } else if (index == 11) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      // Handle the backspace button tap
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
-                    ),
-                    child: Text('<'),
-                  );
-                } else {
-                  return ElevatedButton(
-                    onPressed: () {
-                      // Handle number button tap
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
-                    ),
-                    child: Text('${index + 1}'), // Adjusted index
-                  );
-                }
+            ElevatedButton(
+              onPressed: () {
+                // Handle the "Request" button tap
               },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0), // Rounded button corners
+                  ),
+                ),
+              ),
+              child: Text('Request'),
             ),
-            SizedBox(height: 16.0), // Add spacing
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle the "Request" button tap
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0), // Rounded button corners
-                      ),
-                    ),
+            ElevatedButton(
+              onPressed: () {
+                // Handle the "Pay" button tap
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0), // Rounded button corners
                   ),
-                  child: Text('Request'),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle the "Pay" button tap
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0), // Rounded button corners
-                      ),
-                    ),
-                  ),
-                  child: Text('Pay'),
-                ),
-              ],
+              ),
+              child: Text('Pay'),
             ),
           ],
         ),
-      ),
+      ],
     );
   }
 }
