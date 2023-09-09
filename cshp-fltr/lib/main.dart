9694607ddacb5a5ec7e49ab3ef33a73ca691616f
kerta1n <36344851+kerta1n@users.noreply.github.com>
2023-09-09 16:01:15 -0400

    update directories

---
+++ cshp-fltr/lib/main.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material 3 Example',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF00D632), // Green
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[900], // Dark background
+          elevation: 0, // Remove the shadow
+          titleTextStyle: TextStyle(
+            color: Colors.white, // Title text color
+          ),
+        ),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onNavItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.fullscreen),
+          onPressed: () {
+            // Handle the fullscreen icon tap
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.account_circle), // Changed to account circle icon
+            onPressed: () {
+              // Handle the account circle icon tap
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          '\$0', // Default number display
+          style: TextStyle(fontSize: 36.0), // Adjust the font size as needed
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '', // Empty label to remove text
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onNavItemTapped,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.grey[900], // Dark background for the navbar
+      ),
+      bottomSheet: Container(
+        color: Colors.grey[900], // Dark background
+        padding: EdgeInsets.all(16.0), // Add padding
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16.0), // Add padding around the currency switcher
+              decoration: BoxDecoration(
+                color: Colors.grey[900], // Dark background
+                borderRadius: BorderRadius.circular(8.0), // Rounded dropdown
+              ),
+              child: DropdownButton<String>(
+                value: 'USD', // Default currency
+                onChanged: (newValue) {
+                  // Handle currency change
+                },
+                items: <String>['USD', 'EUR', 'GBP', 'JPY', 'CAD']
+                    .map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0, // Square buttons
+              ),
+              shrinkWrap: true,
+              itemCount: 12, // Added buttons 0, 9, and '<'
+              itemBuilder: (context, index) {
+                if (index == 9) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // Handle the "0" button tap
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    ),
+                    child: Text('0'),
+                  );
+                } else if (index == 10) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // Handle the "." button tap
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    ),
+                    child: Text('.'),
+                  );
+                } else if (index == 11) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // Handle the backspace button tap
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    ),
+                    child: Text('<'),
+                  );
+                } else {
+                  return ElevatedButton(
+                    onPressed: () {
+                      // Handle number button tap
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    ),
+                    child: Text('${index + 1}'), // Adjusted index
+                  );
+                }
+              },
+            ),
+            SizedBox(height: 16.0), // Add spacing
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle the "Request" button tap
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0), // Rounded button corners
+                      ),
+                    ),
+                  ),
+                  child: Text('Request'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle the "Pay" button tap
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[900]), // Dark background
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0), // Rounded button corners
+                      ),
+                    ),
+                  ),
+                  child: Text('Pay'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
